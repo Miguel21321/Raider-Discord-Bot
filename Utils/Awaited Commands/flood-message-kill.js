bd9af5e4f3baf57ef0e67905d59fa1a610324a76
Huguitis <65133800+Huguitis@users.noreply.github.com>
2021-07-05 00:04:48 +0200

    Update flood-message-kill.js

--- Utils/Awaited Commands/flood-message-kill.js
+++ Utils/Awaited Commands/flood-message-kill.js
@@ -2,8 +2,7 @@ module.exports = ({
 name: "flood-message-kill",
 type: "awaitedCommand",
 code: `
-@everyone FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED FUCKED 
-https://images2.imgbox.com/03/9a/nj8PuwKv_o.gif
+@everyone FUCKED $image[https://imagizer.imageshack.com/img923/6253/PnbTER.gif]
 $suppressErrors
 ` 
 })
